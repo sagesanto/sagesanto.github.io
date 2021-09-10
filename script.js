459de1e6d0d2bf9a437210163ae8f8931ae479a5
santomennam <54360977+santomennam@users.noreply.github.com>
2021-09-10 12:10:00 -0400

    spruced up 404 page

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-function flashbang(callingElement)
+async function flashbang(callingElement)
 {
+    callingElement.disabled = true;
+    await sleep(getRandomInt(15000))
     let myModal = document.getElementById('myModal');
     myModal.style.display = "block";
-    console.log(callingElement);
-    callingElement.disabled = true;
+    console.log(callingElement);   
     var flash = new Audio('audio1.mp3');
     audio.play();
     audio.addEventListener('ended', (event) => {
         callingElement.disabled = false;
         myModal.style.display = "none";
     });
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
